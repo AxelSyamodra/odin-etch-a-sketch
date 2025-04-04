3d48191a5103d3512da707e62a163fdadd0b63e7
Syamodra <codesyamodra@gmail.com>
2025-04-04 13:27:14 +0700

    Add the "draw" effect
    Creating a hover effect to the box to simulate the drawing by adding event listener to the box

--- script.js
+++ script.js
@@ -3,5 +3,10 @@ const container = document.querySelector(".container");
 for (let i = 0; i < 256; i++) {
   const box = document.createElement("div");
   box.classList.add("box");
+
+  box.addEventListener("mouseover", () => {
+    box.classList.add("active");
+  });
+
   container.append(box);
 }
