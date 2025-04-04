80eed2869a8eebcd31f5cd423fd9c25c19b90a02
Syamodra <codesyamodra@gmail.com>
2025-04-04 14:00:47 +0700

    Adding clear grid and new grid function
    Create a button that can clear the grid to start over. Create a button to create a new grid with size based on input

--- script.js
+++ script.js
@@ -1,12 +1,43 @@
 const container = document.querySelector(".container");
+const button = document.querySelector(".newGridBtn");
+const buttonClear = document.querySelector(".clearBtn");
 
-for (let i = 0; i < 256; i++) {
-  const box = document.createElement("div");
-  box.classList.add("box");
+function createGrid(size) {
+  container.innerHTML = ""; // Clear previous grid
+  const boxSize = 640 / size;
 
-  box.addEventListener("mouseover", () => {
-    box.classList.add("active");
-  });
+  for (let i = 0; i < size * size; i++) {
+    const box = document.createElement("div");
 
-  container.append(box);
+    box.classList.add("box");
+    box.style.width = `${boxSize}px`;
+    box.style.height = `${boxSize}px`;
+
+    box.addEventListener("mouseover", () => {
+      box.classList.add("active");
+    });
+
+    container.append(box);
+  }
 }
+
+button.addEventListener("click", () => {
+  let newSize = prompt("Enter number of squares per side (max 100):");
+
+  if (newSize !== null) {
+    newSize = parseInt(newSize);
+
+    if (!isNaN(newSize) && newSize > 0 && newSize <= 100) {
+      createGrid(newSize);
+    } else {
+      alert("Please enter a number between 1 and 100.");
+    }
+  }
+});
+
+buttonClear.addEventListener("click", () => {
+  const boxes = document.querySelectorAll(".box");
+  boxes.forEach((box) => box.classList.remove("active"));
+});
+
+createGrid(16); /* initial grid 16x16 */
